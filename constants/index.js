f6c118c44a1e19096a4573c46181c8295fd0f770
AbdallahAli2 <119799728+AbdallahAli2@users.noreply.github.com>
2024-04-03 17:42:46 +0100

    fc

---
+++ constants/index.js
@@ -0,0 +1,55 @@
+export const categories = [
+  "Bariis",
+  "Baasto",
+  "Suqaar",
+  "Shah",
+  "Qahwo",
+  "Dorsho",
+  "Cabitaan"
+]
+
+export const foodItems = [
+  {
+    name: 'Bariis',
+    price: '£4.99',
+    ingredients: 'Rice, Chicken Thigh, Salad',
+    desc: 'A mouthwatering plate of bariis is a culinary wonder that delights your senses and satisfies your hunger. The crispy crust is the foundation of this masterpiece, topped with a tangy tomato sauce that perfectly balances the flavors.',
+    image: require('../assets/images/cooked-rice.png'), 
+  },
+  {
+    name: 'Baasto',
+    price: '£6.49',
+    ingredients: 'Pasta, Basil, Tomato Passata',
+    desc: 'Our pasta here at Safari is a culinary masterpiece that delights the palate and leaves you longing for another forkful. Al dente noodles, bathed in a rich, flavorful sauce, form the foundation of this dish. It is adorned with freah basil, sweet tomato passata, and Parmesan that is guaranteed to make your tastebuds tingle. ',
+    image: require('../assets/images/spaghetti.png'),
+  },
+  {
+    name: 'Shah or Qahwo',
+    price: '£1.79',
+    ingredients: 'Tea or Coffee, Cardamom, Ginger, Pistachio',
+    desc: "A soothing cup of tea is a delightful experience that warms the soul and invigorates the senses. Fragrant pistachio, aromatic cardamom, and spicy ginger blend together harmoniously, infusing the tea with a rich and exotic flavor. Sipping this comforting brew transports you to a world of tranquility and contentment, making each moment a serene and enjoyable one.",
+    image: require('../assets/images/hot-beverage.png'),
+  },
+  {
+    name: 'Suqaar',
+    price: '£28.49',
+    ingredients: 'Lamb, Wagyu, Peppercorns',
+    desc: 'Somali meat stew is a culinary delight that satisfies the appetite and fills the air with irresistible aromas. Tender pieces of lamb and beef simmer to perfection in a savory broth, infused with the bold flavors of peppercorns and a medley of aromatic spices. Each spoonful is a comforting embrace of warmth and flavor, transporting you to a world of culinary bliss with every savory bite.',
+    image: require('../assets/images/pot-of-food.png'),
+  },
+  {
+    name: 'Cabitaan',
+    price: '99p',
+    ingredients: 'Read description inside',
+    desc: 'You have multiple choices of drink. Fanta, Coca-Cola, our famous Mocktails, Red Bull, Mountain Dew, and Pepsi are all viable options.',
+    image: require('../assets/images/cup-with-straw.png'),
+  },
+  {
+    name: 'Dorsho',
+    price: '£6.49',
+    ingredients: 'Flour, EGGS, Sugar',
+    desc: 'Indulge in the decadence of a cake that is unlike any other. Picture this: layers of moist sponge, delicately infused with the essence of chocolate, form the foundation of this culinary masterpiece. As you take a bite, you are greeted by the luxurious embrace of creamy icing, adding a touch of sweetness to the symphony of flavors. It is a delightful blend of textures and tastes, each mouthful a journey of pure bliss and satisfaction.',
+    image: require('../assets/images/shortcake.png'),
+  }
+
+]
